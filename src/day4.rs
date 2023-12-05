b012ca0a2fb7b19bb788d3e793403d25648c23fc
Mirco <mirco.entw@gmail.com>
2023-12-05 23:48:58 +0100

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{cmp, collections::VecDeque, fs};
 
 pub(crate) fn first() -> u32 {
     let data = fs::read_to_string("./src/data/day4.txt").expect("error opening file");
@@ -17,7 +17,46 @@ pub(crate) fn first() -> u32 {
                 .filter(|my_num| winning_nums.contains(my_num))
                 .count() as u32;
 
-            (count > 0).then(|| 2_u32.pow(count - 1))
+            (count > 0).then(|| 2u32.pow(count - 1))
         })
         .sum()
+}
+
+pub(crate) fn second() -> u32 {
+    let data = fs::read_to_string("./src/data/day4.txt").expect("error opening file");
+
+    data.lines()
+        .fold(
+            (0, VecDeque::new() as VecDeque<u32>),
+            |(sum, mut copies_tracker), line| {
+                let sanitized_line = line.replace("  ", " ");
+                let (_, numbers) = sanitized_line.split_once(": ").unwrap();
+                let (winning, mine) = numbers.split_once(" | ").unwrap();
+
+                let winning_nums: Vec<&str> = winning.split_whitespace().collect();
+                let my_nums: Vec<&str> = mine.split_whitespace().collect();
+
+                let instances_of_current_card = copies_tracker.pop_front().unwrap_or(0) + 1;
+
+                let new_copies: VecDeque<u32> = my_nums
+                    .into_iter()
+                    .filter_map(|my_num| {
+                        winning_nums
+                            .contains(&my_num)
+                            .then_some(instances_of_current_card)
+                    })
+                    .collect();
+
+                if new_copies.len() > copies_tracker.len() {
+                    copies_tracker.resize(new_copies.len(), 0)
+                }
+                copies_tracker
+                    .iter_mut()
+                    .enumerate()
+                    .for_each(|(i, item)| *item += new_copies.get(i).unwrap_or(&0));
+
+                return (sum + instances_of_current_card, copies_tracker);
+            },
+        )
+        .0
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     // println!("Day 1 Part 1: {}", day1::first());
@@ -9,6 +10,10 @@ fn main() {
     // println!("Day 2 Part 1: {}", day2::first());
     // println!("Day 2 Part 2: {}", day2::second());
 
-    println!("Day 3 Part 1: {}", day3::first());
+    // println!("Day 3 Part 1: {}", day3::first());
     // println!("Day 3 Part 2: {}", day3::second());
+    
+    // println!("Day 4 Part 1: {}", day4::first());
+    println!("Day 4 Part 2: {}", day4::second());
+
 }
